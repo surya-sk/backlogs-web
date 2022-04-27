99cc29355b08444da4936104cf470bd7c7092632
surya-sk <surya.sk05@outlook.com>
2022-04-27 11:17:17 -0300

    Show search results

---
+++ SearchResult.cs
@@ -0,0 +1,10 @@
+﻿namespace BacklogsWeb
+{
+    public class SearchResult
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? ImageURL { get; set; }
+    }
+}
